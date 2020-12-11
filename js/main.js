106f5d4715c7e5f3fe74ca94e555f376d5bac9bb
Adrita Rahman Tory <45292117+sadgirltory@users.noreply.github.com>
2020-12-11 17:14:45 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const responsive ={
+	0:{
+		items:1
+	},
+	320: {
+		items:1
+	},
+	560:{
+		items: 2
+	},
+	960:{
+		items: 3
+	}
+}
+
+
+$(document).ready(function(){
+
+	$nav = $('.nav');
+	$toggleCollapse = $('.toogle-collapse');
+
+
+	/** click event on toggle menu */
+
+	$toggleCollapse.click(function(){ 
+		$nav.toggleClass('collapse');//method. here i can add and remove multile css file
+	})
+
+
+	//owl carousel for blog
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		autoplay: true,
+	//	autoplayTimeout: 3000,
+		dots:false,
+		nav: true,
+		navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+		responsive: responsive
+	});
+
+
+	//click to scroll top
+
+	$('.move-up span').click(function(){
+		$('html,body').animate({
+			scrollTop: 0
+		}, 1000);
+	})
+
+
+
+	//animation of scroll instance
+
+	AOS.init();
+
+});
